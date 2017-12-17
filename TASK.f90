109f4f3c0e55c3f082238f8df1d9a1674f03e9b2
Roustique <rustam-crocodile@mail.ru>
2017-12-18 01:34:39 +0300

    Release 1.2

--- TASK.f90
+++ TASK.f90
@@ -1,61 +1,61 @@
 module Homework
 use :: omp_lib
+implicit none
 contains
 subroutine FindMaxCoordinates(A, x1, y1, x2, y2)
 real(8), intent(in), dimension(:,:) :: A
 real(8), dimension(size(A(:,1))) :: B
 integer(4), intent(out) :: x1, y1, x2, y2
-integer(4) tx1, tx2, ty1, ty2
-integer(4) n, m, i, j, k, minn, maxx, x, y
-real(8) pr, S, maxS, tmaxS
+integer(4) thread_x1, thread_x2, thread_y1, thread_y2
+integer(4) Alength, Aheight, i, j, k, bottom_border, upper_border, x, y
+real(8) previous_Summ, Summ, maxSumm, thread_maxSumm
 
-X=0; tx1=1; tx2=1; ty1=1; ty2=1; maxS=A(1,1); tmaxS=A(1,1)
-m=size(A(:,1))
-n=size(A(1,:))
-!$omp parallel default(firstprivate) shared(A, x1, x2, y1, y2, maxS)
-do i=(omp_get_thread_num()+1),n,omp_get_num_threads()
+thread_x1=1; thread_x2=1; thread_y1=1; thread_y2=1; maxSumm=A(1,1); thread_maxSumm=A(1,1)
+Aheight=size(A(:,1))
+Alength=size(A(1,:))
+!$omp parallel default(firstprivate) shared(A, x1, x2, y1, y2, maxSumm)
+do i=(omp_get_thread_num()+1),Alength,omp_get_num_threads()
 
- do k=1,m
+ do k=1,Aheight
   B(k)=0
  enddo
 
- do j=i,n
+ do j=i,Alength
   B=B+A(:,j)
-  pr=B(1); x=1; y=1
-  S=pr; minn=x; maxx=y
-  do k=2,m
-   if (B(k)>(B(k)+pr)) then
+  previous_Summ=B(1); x=1; y=1
+  Summ=previous_Summ; bottom_border=x; upper_border=y
+  do k=2,Aheight
+   if (B(k)>(B(k)+previous_Summ)) then
     x=k
     y=k
-    pr=B(k)
+    previous_Summ=B(k)
    else
     y=k
-    pr=B(k)+pr
+    previous_Summ=B(k)+previous_Summ
    endif
-   if (pr>S) then
-    S=pr
-    minn=x
-    maxx=y
+   if (previous_Summ>Summ) then
+    Summ=previous_Summ
+    bottom_border=x
+    upper_border=y
    endif
    !write(*,*)B,S
   enddo
-  if (S>tmaxS) then
-   tmaxS=S
-   tx1=i
-   tx2=j
-   ty1=minn
-   ty2=maxx
+  if (Summ>thread_maxSumm) then
+   thread_maxSumm=Summ
+   thread_x1=i
+   thread_x2=j
+   thread_y1=bottom_border
+   thread_y2=upper_border
   endif
  enddo
- 
 enddo
   !$omp critical
-  if ((tmaxS>maxS).AND.(tx1/=0)) then
-   maxS=tmaxS
-   y1=tx1
-   y2=tx2
-   x1=ty1
-   x2=ty2
+  if (thread_maxSumm>maxSumm) then
+   maxSumm=thread_maxSumm
+   y1=thread_x1
+   y2=thread_x2
+   x1=thread_y1
+   x2=thread_y2
   endif
   !$omp end critical
 !$omp end parallel
